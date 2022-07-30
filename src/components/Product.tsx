df078087d9dc229da62027ca23f390dad71f90a6
Nikolay <nikolay.kovzik@gmail.com>
2022-07-30 23:13:57 +0300

    fetching data with axios & use useEffect hook

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -2,28 +2,24 @@ import { useState } from "react";
 import { ProductProps } from "../models";
 
 
-
 export function Product({ product }: ProductProps) {
     const [details, setDetails] = useState(false);
+    const btnBgClassName = details ? 'bg-blue-400' : ' bg-yellow-400';
+    const btnClasses = ['py-2', 'px-4', 'border', btnBgClassName];
     return (
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
             <img className="w-1/6" src={product.image} alt={product.title}></img>
             <p>{product.title}</p>
             <p className="font-bold">{product.price}</p>
-            {!details && <button
-                className="py-2 px-4 border bg-yellow-400"
-                onClick={() => setDetails(true)}
+            <button
+                className = {btnClasses.join(' ')}
+                onClick={() => setDetails(!details)}
             >
-                Show Details
-            </button>}
-            {details && <button
-                className="py-2 px-4 border bg-blue-400"
-                onClick={() => setDetails(false)}
-            >
-                Hide Details
-            </button>}
+                {details ? `Hide Details` : `Show Details`}
+            </button>
             {details && <div>
                 <p>{product.description}</p>
+                <p>Rate: <span style={{fontWeight: "bold"}}>{product.rating.rate}</span></p>
             </div>}
         </div>
     )
